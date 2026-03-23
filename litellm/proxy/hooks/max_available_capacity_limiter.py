fea21d0d4c3581c9785f725005fde38579bc4dbc
xmartis3 <martiska14lukas@gmail.com>
2026-03-23 15:10:07 +0100

    log format outputs

--- litellm/proxy/hooks/max_available_capacity_limiter.py
+++ litellm/proxy/hooks/max_available_capacity_limiter.py
@@ -6,13 +6,13 @@ from litellm._logging import verbose_proxy_logger
 from litellm.caching.caching import DualCache
 from litellm.integrations.custom_logger import CustomLogger
 from litellm.proxy._types import UserAPIKeyAuth
-from litellm.proxy.utils import InternalUsageCache
+
 
 
 class _PROXY_MaxAvailableCapacityLimiter(CustomLogger):
     # Class variables or attributes
-    def __init__(self, internal_usage_cache: InternalUsageCache):
-        self.internal_usage_cache = internal_usage_cache
+    def __init__(self):
+        pass
         
 
     async def async_pre_call_hook(
@@ -23,8 +23,8 @@ class _PROXY_MaxAvailableCapacityLimiter(CustomLogger):
         call_type: str,
     ):
         verbose_proxy_logger.debug("Inside Max Available Capacity Limiter Pre-Call Hook")
-        verbose_proxy_logger.debug("data:", data)
-        verbose_proxy_logger.debug("call_type:", data)
+        verbose_proxy_logger.debug(f"data: {data}" )
+        verbose_proxy_logger.debug(f"call_type: {data}")
         
         await self.db_call()
         
@@ -33,7 +33,7 @@ class _PROXY_MaxAvailableCapacityLimiter(CustomLogger):
     async def db_call(self):
         from litellm.proxy.proxy_server import prisma_client
         db_resp = await self.get_generated_toknes_by_model_and_time("deepseek-v3.2", prisma_client)
-        verbose_proxy_logger.debug("db_resp", db_resp)
+        verbose_proxy_logger.debug(f"db_resp: {db_resp}")
 
 
     async def get_generated_toknes_by_model_and_time(self, model: str, prisma_client):
